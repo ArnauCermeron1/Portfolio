f9ec8d7e2e192472f5e7cfd72d60b2ec39d45b0f
ArnauCermeron <arnaucermeron@gmail.com>
2025-01-25 23:30:19 +0100

    Basic Info

---
+++ resources/js/Pages/Index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Hello, I am Arnau Cermeron Gallego</h1>
+    <p>Software Engineer</p>
+    <p>Back-End Developer</p>
+    <br>
+    <p>📍 Barcelona, Spain</p>
+    <a href="mailto:arnaucermeron@gmail.com">Email</a>
+  </div>
+</template>
